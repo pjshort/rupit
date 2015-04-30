b249c2901197ceb42b7b9f48709b03c0cd69105b
Patrick Short <ps14@sanger.ac.uk>
2015-04-30 12:12:08 +0100

    changing path to regulatory mutation null model data

--- R/regmut_null_model.R
+++ R/regmut_null_model.R
@@ -3,13 +3,13 @@
 # Probability of mutation in middle base of each trinucleotide is calculated from empirical data.
 # The probabilities are assumed poisson, independent and summed across each sequence
 
-mut_rates <- read.table("/Users/ps14/code/DDD/denovo_regs/data/forSanger_1KG_mutation_rate_table.txt", header=TRUE)
+mut_rates <- read.table("../data/forSanger_1KG_mutation_rate_table.txt", header=TRUE)
 
 ### SNP MODEL - based on sequence context ####
 
 # the indel null model is inferred directly from global snp mutation rate and data - see rupit_core.R for generate_indel_null function
 
-p_all <- function(from){
+p_base <- function(from){
   
   # probability of mutation from base in position 2 to any of three other possible bases
 
@@ -17,12 +17,24 @@ p_all <- function(from){
   return(sum(p))
 }
 
+p_position <- function(sequence, normalize = FALSE){
+  
+  # return vector length nchar(sequence) - 2 with probability of mutation at each base
+  
+  sequence = as.character(sequence)
+  p = sapply(seq(1:nchar(sequence)-2), function(z) p_base(substr(sequence, z, z+2)))
+  if (normalize == FALSE){
+    return(p)
+  } else {
+    return(p/sum(p))
+  }
+}
+
 p_sequence <- function(sequence){
   
   # sum p_all across each trinucleotide sliding along sequence
   
-  sequence = as.character(sequence)
-  p = lapply(seq(1:nchar(sequence)-2), function(z) p_all(substr(sequence, z, z+2)))
+  p = p_position(sequence)
   return(sum(as.numeric(p)))
   
 }
